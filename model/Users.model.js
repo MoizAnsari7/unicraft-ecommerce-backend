36c4910a51488bdebf46f1d06ee1bf56b84f19b4
MoizAnsari7 <moizansari176@gmail.com>
2024-11-08 15:05:02 -0500

    Users Model Added

---
+++ model/Users.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+// User Schema
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+  role: { type: String, default: 'Customer' },
+  addresses: [
+    {
+      _id: mongoose.Schema.Types.ObjectId,
+      street: String,
+      city: String,
+      state: String,
+      zip: String,
+      country: String,
+      phone: String,
+      default: { type: Boolean, default: false }
+    }
+  ],
+  orderHistory: [{ orderId: mongoose.Schema.Types.ObjectId }],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
